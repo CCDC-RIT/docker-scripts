ef829e35f27b344e960ece9ed8675d0d9cb0316f
Eshan <me3031@rit.edu>
2022-03-23 14:04:29 -0400

    more pseudocode

--- docker_backup.sh
+++ docker_backup.sh
@@ -3,3 +3,11 @@
 # We stay backing up 
 # https://docs.docker.com/desktop/backup-and-restore/
 
+# Create Secret Directory 
+mkdir -p /usr/share/docker-backup
+
+# Commit containers (1)
+# ex: docker container commit --> docker-backup 
+
+# Docker Push (2) 
+# ex: docker push 
---
+++ docker_restore.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Get all container IDs 
+
+# Docker Restore 
+# Consider backing up to a local tar file? Might be more secure??
+
