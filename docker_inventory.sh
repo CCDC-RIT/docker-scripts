29afae0ece1bec9bbff0948bc6023ea5328471ce
Eshan <me3031@rit.edu>
2022-03-23 14:35:02 -0400

    added some inventory commands

--- docker_inventory.sh
+++ docker_inventory.sh
@@ -4,18 +4,39 @@
 # https://docs.docker.com/engine/reference/commandline/inspect/
 
 # Get ContainerID
+echo "*----* CONTAINER ID *----*"
+
+CONTAINERID= docker ps -a | tail -n +2 | awk '{print $1}'
+
+echo $CONTAINERID
 
 # Get Container Name 
+echo "*----* CONTAINER NAME *----*"
+
+CONTAINER_NAME= docker ps -a | tail -n +2 | awk '{print $2}'
+
+echo $CONTAINER_NAME
 
 # Get Command
+echo "*----* SHELL *-----*"
+
+CONTAINER_SHELL= docker ps -a | tail -n +2 | awk '{print $3}'
+
+echo $CONTAINER_SHELL
 
 # Get Created 
+echo "---- CREATED ----"
+
 
 # Status(es)
+echo "---- STATUS ----"
+
 
 # Ports 
+echo "---- PORTS ----"
 
 # Exposed Ports 
+echo "---- EXPOSED PORTS ----"
 
 # Inspect Docker Container(s) 
 # Grabs LogPath and Most Recent container logs
