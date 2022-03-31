60e163acea913dd8c2d0e835b1f6798eaee05b2e
Eshan <me3031@rit.edu>
2022-03-31 08:50:50 -0400

    Update docker_backup.sh

--- docker_backup.sh
+++ docker_backup.sh
@@ -1,13 +1,46 @@
 #!/bin/bash
 
-# We stay backing up 
-# https://docs.docker.com/desktop/backup-and-restore/
+backup_path="~/home/core/docky"
 
-# Create Secret Directory 
-mkdir -p /usr/share/docker-backup
+tar_opt="--exclude='/var/run*'"
 
-# Commit containers (1)
-# ex: docker container commit --> docker-backup 
+cd "${BASH_SOURCE%/*}" || exit 
 
-# Docker Push (2) 
-# ex: docker push 
+mkdir -p $backup_path
+
+echo "Starting docker backup..."
+
+echo "" 
+
+echo "Backing up images..."
+
+echo ""
+
+for i in `docker inspect --format='{{.Name}}' $(docker ps -q) | cut -f2 -d\/`
+        do container_name=$i
+        echo -n "$container_name - "
+        container_image=`docker inspect --format='{{.Config.Image}}' $container_name`
+        mkdir -p $backup_path/$container_name
+        save_file="$backup_path/$container_name/$container_name-image.tar"
+        docker save -o $save_file $container_image
+        echo "OK"
+done
+
+echo "Backing up volumes..."
+
+for i in `docker inspect --format='{{.Name}}' $(docker ps -q) | cut -f2 -d\/`
+        do container_name=$i
+        mkdir -p $backup_path/$container_name
+        echo -n "$container_name - "
+	docker run --rm --userns=host \
+  	--volumes-from $container_name \
+  	-v $backup_path:/backup \
+  	-e TAR_OPTS="$tar_opts" \
+  	ritccdc/docker-backup \
+        backup "$container_name/$container_name-volume.tar.xz"
+	echo "OK"
+done
+
+echo "Done. Fuck off."
+
+echo ""
