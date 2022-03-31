8854ac34cde24fc7222d885b4ea0d90533eb408a
Eshan <me3031@rit.edu>
2022-03-31 15:01:32 -0400

    bro bro

--- Ansible /install-ansible.sh
+++ Ansible /install-ansible.sh
@@ -2,5 +2,24 @@
 
 echo "Install Ansible"
 
-echo "Which Operating System are you on?"
+PS3='Which Operating System are you on?: '
+
+choice=("1 - CentOS/RHEL/Fedora" "2 - Debian/Ubuntu" "Quit")
+
+select $ch in "${choice[@]}"
+do
+    case $ch in
+    "1 - CentOS/RHEL/Fedora")
+        echo "INSERT CODE HERE"
+        ;;
+    "2 - Debian/Ubuntu")
+        echo "INSERT CODE HERE"
+        ;;
+    "Quit")
+        break
+        ;;
+    *)  echo "invalid option $REPLY";;
+    esac
+done
+
 
